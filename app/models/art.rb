5f2b5f3f1556f83ba3209e36b020cf8e0f88a584
Fabio Panettieri <fpanettieri@gmail.com>
2018-09-03 21:17:55 -0300

    default images

--- app/models/art.rb
+++ app/models/art.rb
@@ -1,4 +1,4 @@
 class Art < ApplicationRecord
-  has_attached_file :img, styles: { medium: "200x300>", img: "64x64>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :img, styles: { medium: "400x600>", thumb: "80x120>" }, default_url: "https://placeimg.com/800/1200/any"
   validates_attachment_content_type :img, content_type: /\Aimage\/.*\z/
 end
