6ddf4626696383d349d8b54dd2eee635088309ff
Fabio Panettieri <fpanettieri@gmail.com>
2018-09-04 21:46:53 -0300

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2018_09_03_153048) do
 
-  create_table "refs", force: :cascade do |t|
+  create_table "collections", force: :cascade do |t|
+    t.string "name"
     t.string "slug"
-    t.integer "votes"
-    t.integer "views"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "img_file_name"
@@ -24,10 +24,10 @@ ActiveRecord::Schema.define(version: 2018_09_03_153048) do
     t.datetime "img_updated_at"
   end
 
-  create_table "collections", force: :cascade do |t|
-    t.string "name"
+  create_table "refs", force: :cascade do |t|
     t.string "slug"
-    t.text "description"
+    t.integer "votes"
+    t.integer "views"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "img_file_name"
