0c2ba8baf06f00b6886dd7268c94e1efcf88298f
Fabio Panettieri <fpanettieri@gmail.com>
2018-09-05 11:55:02 -0300

    removed actioncable + collections screen

---
+++ app/models/concerns/sluggable.rb
@@ -0,0 +1,12 @@
+module Sluggable
+  extend ActiveSupport::Concern
+
+  included do
+    before_validation :generate_slug
+
+    private
+      def generate_slug
+        self.slug = SecureRandom.urlsafe_base64 8 if self.slug.blank?
+      end
+  end
+end
--- app/models/ref.rb
+++ app/models/ref.rb
@@ -1,4 +1,6 @@
 class Ref < ApplicationRecord
+  include Sluggable
+  
   has_attached_file :img, styles: { medium: "400x600>", thumb: "80x120>" }, default_url: "https://placeimg.com/400/600/any"
   validates_attachment_content_type :img, content_type: /\Aimage\/.*\z/
 end
