634388f6400a753dcce34172f6d75c792aaf3b98
Fabio Panettieri <fpanettieri@gmail.com>
2018-09-05 22:56:19 -0300

    working collection creation

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,6 +2,6 @@ class Collection < ApplicationRecord
   include Sluggable
   include Secretable
 
-  has_attached_file :img, styles: { medium: "400x600>", thumb: "80x120>" }, default_url: "https://placeimg.com/400/600/any"
+  has_attached_file :img, styles: { medium: "400x600>" }, default_url: "https://placeimg.com/400/600/any"
   validates_attachment_content_type :img, content_type: /\Aimage\/.*\z/
 end
