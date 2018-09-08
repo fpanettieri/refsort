e2fecfa82175d470cd8c3739317b51b806f5e01d
Fabio Panettieri <fpanettieri@gmail.com>
2018-09-08 16:09:37 -0300

    new collection notification

--- test/mailers/previews/collection_mailer_preview.rb
+++ test/mailers/previews/collection_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/collection_mailer
 class CollectionMailerPreview < ActionMailer::Preview
-
+  def new_collection_email
+    @collection = Collection.first
+    CollectionMailer.new_collection_email(@collection.name, @collection.description, @collection.secret, 'test@mailinator.com')
+  end
 end
