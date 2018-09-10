c724291d3ee0ae9a3cf35efb8df25f4fa734c0d0
Fabio Panettieri <fpanettieri@gmail.com>
2018-09-09 22:08:24 -0300

    SMTP config test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,17 +34,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
-
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:              ENV['SMTP_ADDRESS'],
-  #   port:                 ENV['SMTP_PORT'],
-  #   domain:               ENV['SMTP_DOMAIN'],
-  #   user_name:            ENV['SMTP_USERNAME'],
-  #   password:             ENV['SMTP_PASSWORD'],
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['SMTP_ADDRESS'],
+    port:                 ENV['SMTP_PORT'],
+    domain:               ENV['SMTP_DOMAIN'],
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
