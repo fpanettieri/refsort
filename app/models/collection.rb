265b6d6d22dc7347645fb6f8b0ad65f9570d5c88
Fabio Panettieri <fpanettieri@gmail.com>
2018-09-11 14:07:21 -0300

    limit length

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,8 +1,11 @@
 class Collection < ApplicationRecord
-  has_many :items, dependent: :destroy
-
   include Sluggable
   include Secretable
+
+  has_many :items, dependent: :destroy
+
+  validates :name, presence: true, length: {maximum: 40}, allow_nil: false
+  validates :description, presence: true, length: {maximum: 100}, allow_nil: true
 
   has_attached_file :img, styles: { medium: "400x600>" }, default_url: "https://placeimg.com/400/600/any"
   validates_attachment_content_type :img, content_type: /\Aimage\/.*\z/
