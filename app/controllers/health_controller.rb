0f023e14be2afe646395182e4c7e9b70fb393297
Fabio Panettieri <fpanettieri@gmail.com>
2018-09-11 05:03:57 -0300

    health check controller

---
+++ app/controllers/health_controller.rb
@@ -0,0 +1,5 @@
+class HealthController < ApplicationController
+  def check
+    head 200
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :collections
+  get  '/health', to: 'health#check'
   get  '/:id', to: 'collections#show'
   root to: 'collections#index'
 end
