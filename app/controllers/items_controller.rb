be0eb7029647514d87df3742639d10f7fa11a292
Fabio Panettieri <fpanettieri@gmail.com>
2018-09-11 23:05:18 -0300

    shorter urls

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,39 @@
+class ItemsController < ApplicationController
+  before_action :by_secret, only: [:edit, :update, :destroy]
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to edit_collection_path(@collection.secret)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @item.update(item_params)
+    redirect_to edit_collection_path(@collection.secret), notice: 'Item updated.'
+  end
+
+  def destroy
+    @item.destroy
+    head 200
+  end
+
+  private
+    def by_secret
+      @collection = Collection.find_by(secret: params[:id])
+      redirect_to root_path if @collection.nil?
+    end
+
+    def item_params
+      params.permit(:name, :img)
+    end
+end
--- db/migrate/20180903153025_create_items.rb
+++ db/migrate/20180903153025_create_items.rb
@@ -2,8 +2,9 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.integer :collection_id, index: true
-      t.string :slug
-      t.string :secret
+      t.string :name
+      t.string :slug, index: true
+      t.string :secret, index: true
       t.integer :votes
       t.integer :views
       t.float :score
